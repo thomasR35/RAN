d7093b23c599057adbbcfa77685042d005f6bc30
thomasR35 <thomasriou.35@gmail.com>
2024-10-11 11:43:29 +0200

    Sass - 1

---
+++ Jour 5-9 Algorithmie & JS/AlgoToJS Exercice/Machine à laver/index.js
@@ -0,0 +1,31 @@
+let niveau = 0
+let temperature =0
+let temps = 0
+
+function remplissage(){
+    while(niveau <= 50l){
+        ouvrirVanneEau()    
+    }
+fermeVanneEau()
+    while(temperature <= 40°){
+        allumerChauffage()
+    }
+stopperChauffage()    
+}
+
+function vidange(){
+    while(niveau > 0){
+        
+    }
+}
+function vidange(){
+    while(niveau > 0){
+        demarrerVidange()
+}
+stopperVidange()
+}
+
+function essorage(){
+    vidange()
+    while (temps )
+}
