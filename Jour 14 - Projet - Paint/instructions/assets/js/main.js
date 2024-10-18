af66d96a5474b5ddf2e0fb0bab8446e7f43a64a9
thomasR35 <thomasriou.35@gmail.com>
2024-10-18 15:16:01 +0200

    MaJ Prettier Project Paint

--- Jour 14 - Projet - Paint/instructions/assets/js/main.js
+++ Jour 14 - Projet - Paint/instructions/assets/js/main.js
@@ -1,33 +1,56 @@
+function selectColor(event) {
+  let colorBox = event.target;
+  let color = colorBox.style.backgroundColor;
 
-function selectColor(event)
-{
-    let colorBox = event.target;
-    let color = colorBox.style.backgroundColor;
-
-    sessionStorage.setItem("selectedColor", color);
+  sessionStorage.setItem("selectedColor", color);
 }
 
-function getSelectedColor()
-{
-    if(sessionStorage.getItem("selectedColor"))
-    {
-        return sessionStorage.getItem("selectedColor");
-    }
+function getSelectedColor() {
+  if (sessionStorage.getItem("selectedColor")) {
+    return sessionStorage.getItem("selectedColor");
+  }
 
-    return null;
+  return null;
 }
 
-function loadPalette(palette)
-{
-    // le code de l'étape 1 se passe ici
+function loadPalette(palette) {
+  // le code de l'étape 1 se passe ici
+  let headerDivs = document.querySelectorAll("header div");
+  for (let i = 0; i < headerDivs.length && i < palette.length; i++)
+    headerDivs[i].style.backgroundColor = palette[i];
 }
 
+window.addEventListener("DOMContentLoaded", function () {
+  // le code de l'étape 2 se passe ici
+  let headerDivs = document.querySelectorAll("header div");
 
-window.addEventListener("DOMContentLoaded", function(){
-    loadPalette(["#22f6f3", "#3daf7e", "#ffffff", "#ec8236", "#a9a7ee", "#ecc606", "#f783f2", "#e89e80"]);
+  for (let i = 0; i < headerDivs.length; i++) {
+    let colorInput = headerDivs[i].querySelector("input[type='color']");
 
-    // le code de l'étape 2 se passe ici
+    colorInput.addEventListener("input", function () {
+      headerDivs[i].style.backgroundColor = colorInput.value;
+    });
 
-    // le code de l'étape 3 se passe ici
+    headerDivs[i].addEventListener("click", function () {
+      let selectedColor = headerDivs[i].style.backgroundColor;
+      sessionStorage.setItem("selectedColor", selectedColor);
+    });
+  }
 
+  // le code de l'étape 3 se passe ici
+  let mainDivs = document.querySelectorAll("main div div");
+
+  for (let i = 0; i < mainDivs.length; i++) {
+    mainDivs[i].addEventListener("click", function () {
+      let selectedColor = getSelectedColor();
+
+      if (selectedColor) {
+        if (mainDivs[i].style.backgroundColor === selectedColor) {
+          mainDivs[i].style.backgroundColor = "";
+        } else {
+          mainDivs[i].style.backgroundColor = selectedColor;
+        }
+      }
+    });
+  }
 });
